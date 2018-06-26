15fb4352f1b7641972da1f1a023b4cc19255339c
TylerNodell <tyler.l.nodell@gmail.com>
2018-06-26 15:33:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested .target')
 }
+
+function increaseRankBy(n) {
+  var arr = document.getElementsByClassName('.ranked-list')
+  for (var i = 0; i < arr.length; i++) {
+    arr[i].innerHTML = parseInt(arr[i].innerHTML) + 1
+  }
+}
